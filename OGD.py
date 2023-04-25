f5c89b309a659c0e278dbab967c47433a054af29
cedric berther <cedric.berther@gmail.com>
2023-04-25 20:48:25 +0200

    Created Groups with same size

--- OGD.py
+++ OGD.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import os
 
-# Den Pfad der Python-Datei erhalten
+# Get the path of the Python file
 script_path = os.path.abspath(__file__)
 
-# Das Verzeichnis der Python-Datei als Arbeitsverzeichnis festlegen
+# Set the directory of the Python file as the working directory
 os.chdir(os.path.dirname(script_path))
 
 # Read the Excel file into a DataFrame
@@ -14,31 +14,36 @@ df = pd.read_excel(file_path)
 columns_to_keep = ["Vorname", "Nachname", "Pfadiname", "Adresse", "Ort", "Hauptebene", "Rollen", "Anrede", "Kantonalverband"]  # List of columns to keep
 df = df[columns_to_keep]
 
-# Neue Spalte "Gruppe" erstellen
+# New column "Gruppe" created
 df["Gruppe"] = np.nan
 
-# Aktualisieren der Werte in der Spalte "Rollen"
+# Update the values in the "Rollen" column
 df["Rollen"] = np.where(df["Rollen"] == "Teilnehmer/-in", "TN",
                        np.where(df["Rollen"].isin(["Klassenlehrer*in", "Kurshelfer*in", "Kursleiter*in"]), "Leiter", df["Rollen"]))
 
-# Liste von Teilnehmer und Leiter erstellen
+# List of participants and leaders
 teilnehmer = df[df["Rollen"] == "TN"]
 leiter = df[df["Rollen"] == "Leiter"]
 
-# Anzahl von Leitern
+# Shuffle the participants randomly
+teilnehmer = teilnehmer.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Number of participants and leaders
+anzahl_teilnehmer = teilnehmer.shape[0]
 anzahl_leiter = leiter.shape[0]
 
-# Gruppieren und Zählen der Anzahl von Personen in jeder Gruppe
-max_personen_Adresse = teilnehmer.groupby("Adresse").size().max()
-max_personen_Ort = teilnehmer.groupby("Ort").size().max()
-max_personen_Hauptebene = teilnehmer.groupby("Hauptebene").size().max()
-max_personen_Kantonalverband = teilnehmer.groupby("Kantonalverband").size().max()
+# Number of groups and participants per group
+anzahl_gruppen = 6
+teilnehmer_pro_gruppe = 4
 
-# Ausgabe der maximalen Anzahl von Personen mit gleichem Ort
-print("Maximale Anzahl von Personen mit gleicher Adresse:", max_personen_Adresse)
-print("Maximale Anzahl von Personen mit gleichem Ort:", max_personen_Ort)
-print("Maximale Anzahl von Personen mit gleicher Hauptebene:", max_personen_Hauptebene)
-print("Maximale Anzahl von Personen mit gleichem Kantonalverband:", max_personen_Kantonalverband)
+# Distribute participants into groups
+for i in range(anzahl_teilnehmer):
+    gruppe_nummer = (i % anzahl_gruppen) + 1  # Group number starts from 1
+    df.at[teilnehmer.index[i], "Gruppe"] = gruppe_nummer
 
-# Ausgabe der Anzahl Leiter (Gruppen)
-print("Anzahl von Leitern:", anzahl_leiter)
+# Group by "Gruppe" and get the count of occurrences
+gruppe_counts = df.groupby("Gruppe").size()
+
+# Print the count of occurrences for each group
+for gruppe, count in gruppe_counts.items():
+    print("Anzahl von Personen in Gruppe {}: {}".format(gruppe, count))
